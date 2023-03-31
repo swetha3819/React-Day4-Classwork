315363db2629ceb9cdad23cbd941f73099449269
swetha3819 <108916586+swetha3819@users.noreply.github.com>
2023-03-31 09:34:08 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Fruitsurvey from './Fruitsurvey'
+export class App extends Component {
+  render() {
+    return (
+      <div>App
+        <Fruitsurvey/>
+      </div>
+    )
+  }
+}
+
+export default App
